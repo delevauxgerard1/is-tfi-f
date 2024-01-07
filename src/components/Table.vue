99214fd8b74a5e1f0c570e0c9c8f7c72e964612d
Agostina Albornoz <Agostinaalbornoz43@gmail.com>
2024-01-07 03:54:03 -0300

    Agregue la columna cantidad y abajo de la tabla puse un total

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,54 +1,78 @@
 <template>
     <div class="relative max-w-6xl mx-auto px-4 sm:px-6 pt-4 md:pt-0">
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-indigo-300 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3 font-bold">
-                            Artículo
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Talle
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Color
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Precio
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Acciones
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="article in articles" :key="article.id"
-                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-indigo-100 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-700 whitespace-nowrap dark:text-white">
-                            {{ article.brand }} - {{ article.name }}
-                        </th>
-                        <td class="px-6 py-4 font-medium text-gray-700">
-                            {{ article.size }}
-                        </td>
-                        <td class="px-6 py-4 font-medium text-gray-700">
-                            {{ article.color }}
-                        </td>
-                        <td class="px-6 py-4 font-medium text-green-500">
-                            ${{ article.price }}
-                        </td>
-                        <td class="px-6 py-4 font-medium text-gray-700">
-                            <font-awesome-icon
-                                @click="openEditModal(article.id, article.brand, article.name, article.size, article.color, article.price)"
-                                class="pr-2" :icon="['fas', 'pen-to-square']"
-                                :style="{ fontSize: '20px', color: '#fbbf24', cursor: 'pointer' }" />
-                            <font-awesome-icon @click="toggleModal(article.id, article.brand, article.name)"
-                                :icon="['fas', 'trash-can']"
-                                :style="{ fontSize: '20px', color: '#ef4444', cursor: 'pointer' }" />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-indigo-300 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3 font-bold">
+                Artículo
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Talle
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Color
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Precio unitario
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Cantidad
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Acciones
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="article in articles" :key="article.id"
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-indigo-100 dark:hover:bg-gray-600">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-700 whitespace-nowrap dark:text-white">
+                {{ article.brand }} - {{ article.name }}
+              </th>
+              <td class="px-6 py-4 font-medium text-gray-700">
+                {{ article.size }}
+              </td>
+              <td class="px-6 py-4 font-medium text-gray-700">
+                {{ article.color }}
+              </td>
+              <td class="px-6 py-4 font-medium text-green-500">
+                ${{ article.price }}
+              </td>
+              <td class="px-6 py-4 font-medium text-gray-700">
+                {{ article.quantity }}
+              </td>
+              <td class="px-6 py-4 font-medium text-gray-700">
+                <font-awesome-icon
+                  @click="openEditModal(article.id, article.brand, article.name, article.size, article.color, article.price)"
+                  class="pr-2" :icon="['fas', 'pen-to-square']"
+                  :style="{ fontSize: '20px', color: '#fbbf24', cursor: 'pointer' }" />
+                <font-awesome-icon @click="toggleModal(article.id, article.brand, article.name)"
+                  :icon="['fas', 'trash-can']"
+                  :style="{ fontSize: '20px', color: '#ef4444', cursor: 'pointer' }" />
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="4"></td>
+              <td class="px-6 py-4 font-medium text-gray-700">
+                Total:
+              </td>
+              <td class="px-6 py-4 font-medium text-gray-700">
+                ${{ getTotal() }}
+              </td>
+            </tr>
+            <tr>
+              <td colspan="6" class="text-right">
+                <button @click="realizarVenta" class="px-4 py-2 bg-blue-500 text-white rounded-md">
+                  Realizar Venta
+                </button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
 
         <!-- Editar -->
         <div class="fixed inset-0 flex items-center justify-center" :class="{ 'hidden': !showEditModal }">
@@ -108,16 +132,16 @@
 import { ref } from 'vue';
 
 const articles = [
-    { id: 1, brand: 'Nike', name: 'Zapatillas Air Max', size: '42', color: 'Negro', price: 120 },
-    { id: 2, brand: 'Adidas', name: 'Remera Estampada', size: 'M', color: 'Blanco', price: 30 },
-    { id: 3, brand: 'Puma', name: 'Short Deportivo', size: 'L', color: 'Azul', price: 25 },
-    { id: 4, brand: 'Converse', name: 'Zapatillas Chuck Taylor', size: '39', color: 'Rojo', price: 80 },
-    { id: 5, brand: 'Vans', name: 'Gorra Clásica', size: 'Único', color: 'Gris', price: 20 },
-    { id: 6, brand: 'New Balance', name: 'Sweater Cuello Redondo', size: 'XL', color: 'Verde', price: 50 },
-    { id: 7, brand: 'Reebok', name: 'Mochila Deportiva', size: 'Único', color: 'Negro', price: 40 },
-    { id: 8, brand: 'Fila', name: 'Pantalón Jogger', size: 'S', color: 'Gris', price: 35 },
-    { id: 9, brand: 'Under Armour', name: 'Camiseta Sin Mangas', size: 'M', color: 'Azul', price: 28 },
-    { id: 10, brand: 'ASICS', name: 'Zapatillas Gel-Kayano', size: '41', color: 'Blanco', price: 150 },
+    { id: 1, brand: 'Nike', name: 'Zapatillas Air Max', size: '42', color: 'Negro', price: 120, quantity:2 },
+    { id: 2, brand: 'Adidas', name: 'Remera Estampada', size: 'M', color: 'Blanco', price: 30 , quantity:3 },
+    { id: 3, brand: 'Puma', name: 'Short Deportivo', size: 'L', color: 'Azul', price: 25, quantity: 1 },
+    { id: 4, brand: 'Converse', name: 'Zapatillas Chuck Taylor', size: '39', color: 'Rojo', price: 80, quantity:1 },
+    { id: 5, brand: 'Vans', name: 'Gorra Clásica', size: 'Único', color: 'Gris', price: 20, quantity:1 },
+    { id: 6, brand: 'New Balance', name: 'Sweater Cuello Redondo', size: 'XL', color: 'Verde', price: 50, quantity:1 },
+    { id: 7, brand: 'Reebok', name: 'Mochila Deportiva', size: 'Único', color: 'Negro', price: 40 , quantity:2},
+    { id: 8, brand: 'Fila', name: 'Pantalón Jogger', size: 'S', color: 'Gris', price: 35, quantity:2 },
+    { id: 9, brand: 'Under Armour', name: 'Camiseta Sin Mangas', size: 'M', color: 'Azul', price: 28, quantity:4 },
+    { id: 10, brand: 'ASICS', name: 'Zapatillas Gel-Kayano', size: '41', color: 'Blanco', price: 150, quantity:4 },
 ];
 
 const showModal = ref(false);
@@ -158,5 +182,14 @@ const saveArticleChanges = () => {
         articles[index] = editedArticle.value;
     }
     closeEditModal();
+};
+
+const getTotal = () => {
+  return articles.reduce((total, article) => total + (article.price * article.quantity), 0).toFixed(2);
+};
+
+const realizarVenta = () => {
+  // Lógica para realizar la venta
+  // Puedes agregar aquí la lógica que necesitas al realizar la venta
 };
 </script>
