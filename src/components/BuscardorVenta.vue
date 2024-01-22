e237d9cb62e7b353063576ea0ca56cae5f1a4146
Gerard Delevaux <delevauxgerard1@gmail.com>
2024-01-21 22:02:05 -0300

    Corrección Agos + flowbite

--- src/components/BuscardorVenta.vue
+++ src/components/BuscardorVenta.vue
@@ -1,4 +1,4 @@
-<template>
+<template>
     <div class="max-lg:grow flex flex-col w-full lg:w-2/5 lg:mr-auto">
         <div class="w-full max-w-xl mx-auto flex flex-col justify-start px-4 sm:px-6 pb-20 lg:pt-4">
             <div class="relative overflow-x-auto shadow-md border-2 sm:rounded-lg">
@@ -7,6 +7,7 @@
                     <label for="default-search"
                         class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
                     <div class="relative">
+
                         <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -14,24 +15,27 @@
                                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="search" 
-                            id="default-search"
-                            v-model="descripcion"
-                            @input="buscarArticulo"
-                            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Buscar artículo" required>
-
-                        <!-- Mostrar el autocompletado debajo del campo de búsqueda -->
-                        <div v-if="resultados.length > 0" class="mt-2 w-full absolute left-0 bg-white dark:bg-gray-700 border border-gray-300 rounded-md z-10">
+                        <fwb-input type="search" id="default-search" v-model="descripcion" @input="buscarArticulo"
+                            placeholder="Escriba nombre del artículo">
+                            <template #prefix>
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
+                                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" />
+                                </svg>
+                            </template>
+                        </fwb-input>
+                        <div v-if="resultados.length > 0"
+                            class="mt-2 w-full absolute left-0 bg-white dark:bg-gray-700 border border-gray-300 rounded-md z-10">
                             <ul>
-                                <li v-for="resultado in resultados" :key="resultado.id" @click="seleccionarArticulo(resultado)" class="p-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 text-black">
-                                    {{ resultado.nombre }}
+                                <li v-for="resultado in resultados" :key="resultado.id"
+                                    @click="seleccionarArticulo(resultado)"
+                                    class="p-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 text-black">
+                                    {{ resultado.descripcion }}
                                 </li>
                             </ul>
                         </div>
 
-                        <button type="button" 
-                            class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
                     </div>
                 </form>
                 <form class="max-w-md mx-auto">
@@ -99,13 +103,14 @@
 <script setup>
 import axios from 'axios';
 import { ref } from 'vue';
+import { FwbInput } from 'flowbite-vue'
 
 const descripcion = ref('');
 const resultados = ref([]);
 
 const buscarArticulo = async () => {
     try {
-        const response = await axios.get(`http://localhost:8080/tfib/buscarPorDescripcion?descripcion=${descripcion.value}`);
+        const response = await axios.get(`http://localhost:8080/tfib/buscarPorDescripcion/${descripcion.value}`);
         resultados.value = response.data;
     } catch (error) {
         console.error(error);
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="relative max-w-full mx-auto px-4 sm:px-6 pt-4 md:pt-0">
+    <div class="relative w-5/6 mx-auto px-4 sm:px-6 pt-4 md:pt-0">
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-indigo-300 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-3 font-bold">
-                            Artículo
+                            Descripción
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Marca
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Color
                         </th>
                         <th scope="col" class="px-6 py-3">
                             Talle
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Color
+                            Cantidad
                         </th>
                         <th scope="col" class="px-6 py-3">
                             Precio
@@ -22,26 +28,31 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="article in articles" :key="article.id"
+                    <tr
                         class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-indigo-100 dark:hover:bg-gray-600">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-700 whitespace-nowrap dark:text-white">
-                            {{ article.brand }} - {{ article.name }}
+                            Ejemplo Descripción
                         </th>
                         <td class="px-6 py-4 font-medium text-gray-700">
-                            {{ article.size }}
+                            Marca
                         </td>
                         <td class="px-6 py-4 font-medium text-gray-700">
-                            {{ article.color }}
+                            Color
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-700">
+                            Talle
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-700">
+                            999
                         </td>
                         <td class="px-6 py-4 font-medium text-green-500">
-                            ${{ article.price }}
+                            $9999
                         </td>
-                        <td class="px-6 py-4 font-medium text-gray-700">
+                        <td class="px-6 py-4 font-medium">
                             <font-awesome-icon
-                                @click="openEditModal(article.id, article.brand, article.name, article.size, article.color, article.price)"
                                 class="pr-2" :icon="['fas', 'pen-to-square']"
                                 :style="{ fontSize: '20px', color: '#fbbf24', cursor: 'pointer' }" />
-                            <font-awesome-icon @click="toggleModal(article.id, article.brand, article.name)"
+                            <font-awesome-icon
                                 :icon="['fas', 'trash-can']"
                                 :style="{ fontSize: '20px', color: '#ef4444', cursor: 'pointer' }" />
                         </td>
@@ -50,127 +61,9 @@
             </table>
         </div>
 
-        <!-- Editar -->
-        <div class="fixed inset-0 flex items-center justify-center" :class="{ 'hidden': !showEditModal }">
-            <div class="absolute inset-0 bg-gray-800 opacity-50"></div>
-            <div class="bg-white p-8 rounded-lg z-10">
-                <div class="text-center p-3 flex-auto justify-center leading-6">
-                    Editar artículo: {{ editedArticle.name }}
-                </div>
-                <div>
-                    <div class="grid grid-cols-2 gap-4">
-                        <div class="col-span-1">
-                            <label for="editBrand" class="block text-sm font-medium text-gray-700">Marca</label>
-                            <input type="text" id="editBrand" v-model="editedArticle.brand"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="John" required>
-                        </div>
-                        <div class="col-span-1">
-                            <label for="editName" class="block text-sm font-medium text-gray-700">Nombre</label>
-                            <input type="text" id="editName" v-model="editedArticle.name"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="John" required>
-                        </div>
-                    </div>
-                    <div class="mt-4 flex justify-end">
-                        <button @click="saveArticleChanges"
-                            class="mr-2 px-4 py-2 bg-green-500 text-white rounded-md">Guardar</button>
-                        <button @click="closeEditModal" class="px-4 py-2 bg-gray-300 rounded-md">Cancelar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Eliminar -->
-        <div class="fixed inset-0 flex items-center justify-center" :class="{ 'hidden': !showModal }">
-            <div class="absolute inset-0 bg-gray-800 opacity-50"></div>
-            <div class="bg-white p-8 rounded-lg z-10">
-                <div class="text-center p-3 flex-auto justify-center leading-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 flex items-center text-red-500 mx-auto"
-                        viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <p>¿Estás seguro de que quieres eliminar el artículo "{{ articleToDelete.brand }} - {{ articleToDelete.name
-                }}"?</p>
-                <div class="mt-4 flex justify-end">
-                    <button @click="deleteArticle" class="mr-2 px-4 py-2 bg-red-500 text-white rounded-md">Eliminar</button>
-                    <button @click="closeModal" class="px-4 py-2 bg-gray-300 rounded-md">Cancelar</button>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import axios from 'axios';
-
-const articles = [
-    { id: 1, brand: 'Nike', name: 'Zapatillas Air Max', size: '42', color: 'Negro', price: 120 },
-    { id: 2, brand: 'Adidas', name: 'Remera Estampada', size: 'M', color: 'Blanco', price: 30 },
-    { id: 3, brand: 'Puma', name: 'Short Deportivo', size: 'L', color: 'Azul', price: 25 },
-    { id: 4, brand: 'Converse', name: 'Zapatillas Chuck Taylor', size: '39', color: 'Rojo', price: 80 },
-    { id: 5, brand: 'Vans', name: 'Gorra Clásica', size: 'Único', color: 'Gris', price: 20 },
-    { id: 6, brand: 'New Balance', name: 'Sweater Cuello Redondo', size: 'XL', color: 'Verde', price: 50 },
-    { id: 7, brand: 'Reebok', name: 'Mochila Deportiva', size: 'Único', color: 'Negro', price: 40 },
-    { id: 8, brand: 'Fila', name: 'Pantalón Jogger', size: 'S', color: 'Gris', price: 35 },
-    { id: 9, brand: 'Under Armour', name: 'Camiseta Sin Mangas', size: 'M', color: 'Azul', price: 28 },
-    { id: 10, brand: 'ASICS', name: 'Zapatillas Gel-Kayano', size: '41', color: 'Blanco', price: 150 },
-];
-
-const showModal = ref(false);
-const articleToDelete = ref({});
-
-const toggleModal = (articleId, brand, name) => {
-    articleToDelete.value = { id: articleId, brand, name };
-    showModal.value = true;
-};
-
-const closeModal = () => {
-    showModal.value = false;
-};
-
-const deleteArticle = () => {
-    const index = articles.findIndex((article) => article.id === articleToDelete.value.id);
-    if (index !== -1) {
-        articles.splice(index, 1);
-    }
-    closeModal();
-};
-
-const showEditModal = ref(false);
-const editedArticle = ref({});
-
-const openEditModal = (articleId, brand, name, size, color, price) => {
-    editedArticle.value = { id: articleId, brand, name, size, color, price };
-    showEditModal.value = true;
-};
-
-const closeEditModal = () => {
-    showEditModal.value = false;
-};
-
-const saveArticleChanges = () => {
-    const index = articles.findIndex((article) => article.id === editedArticle.value.id);
-    if (index !== -1) {
-        articles[index] = editedArticle.value;
-    }
-    closeEditModal();
-};
-/*
-const url = "http://localhost:8080/tfib/empleados";
-
-const fetchDataWithAxios = async () => {
-    try {
-        const response = await axios.get(url);
-        console.log('Datos recibidos:', response.data);
-    } catch (error) {
-        console.error('Hubo un problema al obtener los datos:', error.message);
-    }
-};
-
-fetchDataWithAxios();*/
 </script>
